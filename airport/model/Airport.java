021a7a3551bab93a4d3936070689863a84c0a959
Konrad Dróżdż <konrdro873@student.polsl.pl>
2024-07-22 14:01:43 +0200

    zad 5

--- airport/model/Airport.java
+++ airport/model/Airport.java
@@ -1,6 +1,8 @@
 package airport.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +10,8 @@ import java.util.List;
 public class Airport {
 
     private int id;
-
+    @NotNull
+    @Size(min = 1, max = 50)
     private String name;
 
     private String logo;
--- airport/webRest/AirportRest.java
+++ airport/webRest/AirportRest.java
@@ -4,13 +4,21 @@ import airport.model.Airport;
 import airport.model.Plane;
 import airport.service.AirportService;
 import airport.service.PlaneService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +28,10 @@ public class AirportRest {
 
         private  final AirportService airportService;
         private final PlaneService planeService;
+        private final MessageSource messageSource;
+        private final LocaleResolver localeResolver;
+
+
 
         @GetMapping("/airports")
         List<Airport> getairports(
@@ -62,8 +74,15 @@ public class AirportRest {
     }
 
     @PostMapping("/airports")
-    ResponseEntity<Airport> addAirport(@RequestBody Airport airport) {
+    ResponseEntity<?> addAirport(@Validated @RequestBody Airport airport, Errors errors, HttpServletRequest request) {
         log.info("about to add new airport: {}", airport);
+        if(errors.hasErrors()) {
+            Locale locale = localeResolver.resolveLocale(request);
+            String errorMessage = errors.getAllErrors().stream()
+                    .map(oe->messageSource.getMessage(Objects.requireNonNull(oe.getCode()), new Object[0], locale))
+                    .reduce("errors:\n", (accu, oe)->accu + oe + "\n");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+        }
         airport = airportService.addAirports(airport);
         log.info("new airport added {}", airport);
         return ResponseEntity.status(HttpStatus.CREATED).body(airport);
